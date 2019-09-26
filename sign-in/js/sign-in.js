0db07a82d5cc9841312adf225b85c976b582e6f1
saburahmed <saburahmed@gmail.com>
2019-09-26 18:09:18 +0100

    update

---
+++ sign-in/js/sign-in.js
@@ -0,0 +1,17 @@
+const signInForm = document.getElementById("signInForm");
+const emailField = document.getElementById("emailField");
+const passField = document.getElementById("passField");
+
+// emailField.addEventListener("keyup", function(event) {
+//   isValidEmail = emailField.checkValidity();
+
+//   if (isValidEmail) {
+//     okButton.disabled = true;
+//   } else {
+//     okButton.disabled = true;
+//   }
+// });
+
+// okButton.addEventListener("click", function(event) {
+//   signInForm.submit();
+// });
